b260077a4f9f2955538b414b0f6f9e4371c7d1a2
anastasiacutulima <anastasia.cutulima4@gmail.com>
2026-03-08 14:44:24 +0200

    am sters fisierele statice, am instalat react in folderul de frontend si am facu pagina pt employee + controller

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "@cloudscape-design/global-styles/index.css";
+import EmployeePage from './pages/EmployeePage';
+
+function App() {
+  return <EmployeePage />;
+}
+
+export default App;
